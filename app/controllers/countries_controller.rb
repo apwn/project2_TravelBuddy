c065cdc025381ea879c85790bde9fb3dd79322ec
apwn <thomas.kanze@gmail.com>
2016-06-02 14:30:50 +1000

    add twitter api

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -38,7 +38,7 @@ class CountriesController < ApplicationController
       @countrypopulation = @country["population"]
       @countrycallingcode = @country["callingCodes"][0]
       @countryarea = @country["area"]
-    
+
     end
 
     gon.capital = @countrycapital
@@ -46,6 +46,13 @@ class CountriesController < ApplicationController
     @cntryname = @countryname.downcase
 
     @flickrimg = HTTParty.get "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f1d62c1433245e6f2024103335abd75a&text=#{ @cntryname } travel&sort=relevance&content_type=1&format=json&nojsoncallback=1"
+
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key    = "GB9dtc5dy9aNBJf3v2S8qu4bx"
+      config.consumer_secret = "IfWqsw17sDcHO1bkS8AShWzVEED867TnxNc1MD8UESmd7XO5An"
+      config.access_token = "3030001322-Dwq4rpNDJyknSBupPdajjCBF3dVoCkfKzZ4TqKH"
+      config.access_token_secret = "hQgKjEfd2xFSxBDBJ84nLNCZJCcdgQ0g6pgDOiQAtetPD"
+    end
   end
 
 end
---
+++ app/helpers/twitter_helper.rb
@@ -0,0 +1,12 @@
+module TwitterHelper
+#
+#     client = Twitter::REST::Client.new do |config|
+#     config.consumer_key    = "GB9dtc5dy9aNBJf3v2S8qu4bx"
+#     config.consumer_secret = "IfWqsw17sDcHO1bkS8AShWzVEED867TnxNc1MD8UESmd7XO5An"
+#     config.access_token = "3030001322-Dwq4rpNDJyknSBupPdajjCBF3dVoCkfKzZ4TqKH"
+#     config.access_token_secret = "hQgKjEfd2xFSxBDBJ84nLNCZJCcdgQ0g6pgDOiQAtetPD"
+#     end
+#
+#
+#
+end
